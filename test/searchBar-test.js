d65b263fa9cd1dc1781783e84530f32fc5302a4e
yang-wei <yangweilim21@gmail.com>
2014-12-14 17:47:36 +0900

    add test for text input and checkbox

---
+++ test/searchBar-test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var assert = require('assert');
+var React = require('react/addons');
+var TestUtils = React.addons.TestUtils;
+var SearchBarComp = require('../components/searchBar.js');
+  
+describe('Search Bar', function() {
+  it('should rendered on DOM', function() {
+    
+    var SearchBar = TestUtils.renderIntoDocument(<SearchBarComp />);
+    
+    var inputs = TestUtils.scryRenderedDOMComponentsWithTag(SearchBar, 'input');
+    // should be [ input[text], input[checkbox] ];
+    assert.equal(inputs.length, 2, "Can't find both input elements");
+    
+    assert.equal(inputs[0].getDOMNode().value, '');
+    assert.equal(inputs[1].getDOMNode().checked, false);
+
+  });
+})
--- test/test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var assert = require('assert');
-
-describe('mocha test', function() {
-  it('should see mocha pass', function() {
-    assert.ok(1);   
-  })
-})
